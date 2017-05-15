a97decc39fb0c360bb2f80182248d00c9c26a03b
rtpike <impressive.rtpike@gmail.com>
2017-05-14 23:31:19 -0700

    Code cleanup

--- app/src/main/java/com/rtpike/setsolver/MainActivity.java
+++ app/src/main/java/com/rtpike/setsolver/MainActivity.java
@@ -35,37 +35,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener, GestureDetector.OnGestureListener {
     //View.OnTouchListener,
     private static final String TAG = ":MainActivity";
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = false;
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private static final int WHAT_PROCESS_IMAGE = 0;
-    private final Handler mHideHandler = new Handler();
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+
     private CameraBridgeViewBase mOpenCvCameraView;
     private processImage mProcessImage;
     private MenuItem mItemDebugCard;
@@ -104,12 +81,6 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         }
     };
     private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
         @Override
@@ -150,7 +121,6 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         // GestureDetector.OnGestureListener
 
         mDetector = new GestureDetectorCompat(this, this);
-        //mDetector.setOnDoubleTapListener(this);
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
@@ -337,29 +307,6 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         return image;
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
 /*
     // result Integer is passed here after
     // this method is run on main UI thread
@@ -367,28 +314,6 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     protected void onPostExecute(Integer result) {
     }
 */
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-
 
     @Override
     public boolean onDown(MotionEvent e) {
